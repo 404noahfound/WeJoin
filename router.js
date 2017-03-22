b6c207db0b26999bc25fc087065c642b44190f8e
jiangyoushizhe <1104481071@qq.com>
2017-03-22 21:55:13 +0800

    update router (for activity)

--- router.js
+++ router.js
@@ -8,11 +8,17 @@ module.exports = function(app){
     app.get('/other', HomeController.Other);
 
     //Activity Routes
+    //Search
+    app.get('/activity/search/:keyword', ActivityController.Search);
+    //View
+    app.get('/activity/:id', ActivityController.View);
     //Create
     app.get('/activity/create', ActivityController.Create);
     app.post('/activity/create', ActivityController.UponCreate);
+    //CustomerModify
+    app.post('/activity/:id/customermodify', ActivityController.CustomerModify);
     //OrganizerModify
-    app.get('/activity/organizermodify', ActivityController.OrganizerModify);
-    app.post('/activity/organizermodify', ActivityController.UponOrganizerModify);
+    app.get('/activity/:id/organizermodify', ActivityController.OrganizerModify);
+    app.post('/activity/:id/organizermodify', ActivityController.UponOrganizerModify);
 
 };
