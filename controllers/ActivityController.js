1ba061e7b480c548f53f92bef10ebfb9f95490ad
HOU <hxltim@163.com>
2017-03-22 21:15:31 +0800

    unfinished activity controller

--- controllers/ActivityController.js
+++ controllers/ActivityController.js
@@ -1,49 +1,84 @@
 const mongoose = require('mongoose');
 const only = require('only');
 const Activity = mongoose.model('Activity');
-
+const User = mongoose.model('User');
+const RegUser = mongoose.model('RegUser');
+const Guest = mongoose.model('Guest');
+const Note = mongoose.model('Note');
+const Notification = mongoose.model('Notification');
+const Request = mongoose.model('Request');
 
 exports.Search = function(request, response){
-	response.pageInfo = {};
-	response.pageInfo.functionality = "Activity.Search"
-	response.render('home/Functionality', response.pageInfo);
+	response.pageInfo.keyword=request.body.keyword;
+	response.pageInfo.resultActivities=Activity.Search(response.pageInfo.keyword);
+	response.pageInfo.functionality = "Activity.Search. Generate page for relevant activitys"
+	response.render('activity/Search', response.pageInfo);
 };
 
+exports.UponSearch = function(request, response){
+	response.pageInfo.title = "Search Result"
+	response.pageInfo.functionality = "Activity.UponSearch";
+	response.render('home/Functionality',response.pageInfo);
+};
 
 exports.View = function(request, response){
-	response.pageInfo = {};
-	response.pageInfo.functionality = "Activity.View"
-	response.render('home/Functionality', response.pageInfo);
+	response.pageInfo.title = "Activity Information"
+	response.pageInfo.functionality = "Activity.View. Generate page for viewing activity"
+	response.pageInfo.activityID=request.params.id;
+	Activity.find({'_id':request.params.id},function(err,docs){
+		if(err) response.pageInfo.err="Error! Can't find the activity page!";
+		response.pageInfo.activities=docs;
+		response.render('activity/View',response.pageInfo);
+	});
+};
+
+exports.UponView = function(request.response){
+	response.pageInfo.functionality = "Activity.UponView."
+	response.render('home/Functionality',response.pageInfo);
 };
 
 exports.Create = function(request, response){
-	response.pageInfo = {};
+	response.pageInfo.title = "Create Activity";
 	response.pageInfo.functionality = "Activity.Create. Generate create account page."
 	response.render('activity/Create', response.pageInfo);
 };
 
 exports.UponCreate = function(request, response){
-	response.pageInfo = {};
-	response.pageInfo.functionality = "Activity.UponCreate";
-	const activity = new Activity(only(request.body, "title description"));
+	response.pageInfo.title="Create success!"
+	response.pageInfo.functionality = "Create success!"
+	const activity = new Activity(only(request.body, "organizer title location time type description "
+		+"expense status rating rated_participants content_for_participants participation_method remind_time "
+		+"participants created_at"));
 	activity.save();
 	response.render('home/Functionality', response.pageInfo);
 };
 
 exports.CustomerModify = function(request, response){
-	response.pageInfo = {};
-	response.pageInfo.functionality = "Activity.CustomerModify. Include Join/Quit/Rate";
-	response.render('home/Functionality', response.pageInfo);
+	response.pageInfo.title= "Customer Modify"
+	response.pageInfo.functionality = "Activity.CustomerModify. Include Join/Quit/Rate"
+	response.render('activity/View',response.pageInfo);
 };
 
 exports.OrganizerModify = function(request, response){
-	response.pageInfo = {};
+	response.pageInfo.title = "OrganizerModify"
 	response.pageInfo.functionality = "Activity.OrganizerModify. Generate page for modifying the activity by organizer."
-	response.render('home/Functionality', response.pageInfo);
+	Activity.find({}, function(err, docs){
+		response.pageInfo.activities = docs;
+		response.render('activity/OrganizerModify', response.pageInfo);
+	});
 };
 
 exports.UponOrganizerModify = function(request, response){
-	response.pageInfo = {};
-	response.pageInfo.functionality = "Activity.Modify"
+	response.pageInfo.functionality = "Activity.UponOrganizerModify";
+	response.render('home/Functionality', response.pageInfo);
+};
+
+exports.GetActivityDescription = function(request, response){
+	response.pageInfo.functionality = "Activity.GetActivityDescription. Generate page for activity description. "
+	response.render('activity/GetActivityDescription',response.pageInfo);
+};
+
+exports.UponGetActivityDescription = function(request,response){
+	response.pageInfo.functionality = "Activity.UponGetActivityDescription"
 	response.render('home/Functionality', response.pageInfo);
 };
