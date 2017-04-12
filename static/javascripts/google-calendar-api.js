2b620787926f94602edda2942dba61e74d3d74c6
Noah Su <noahsuhk@gmail.com>
2017-04-12 22:44:13 +0800

    finetune

--- static/javascripts/google-calendar-api.js
+++ static/javascripts/google-calendar-api.js
@@ -19,6 +19,7 @@ function initClient() {
 }
 function updateSigninStatus(isSignedIn) {
   var cbutton = $('#google-calendar-button');
+  cbuton.off('click');
   if (isSignedIn) {
     cbutton.click(function(){
       // console.log('signed in!');
